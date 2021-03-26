db59afc8d2294853942f589726c2a05d51f8f344
Jimmy McBroom <james.mcbroom@gmail.com>
2021-03-26 16:38:13 -0400

    buttons can be toggle now

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,8 +1,8 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-const Button = ({ text, icon, onClick, disabled=false}) => {
+const Button = ({ text, icon, onClick, disabled=false, active=true}) => {
   return (
-    <button className='flex items-center btn-enabled my-2' onClick={onClick}>
+    <button className={active ? 'flex items-center btn-enabled my-2' : 'flex items-center btn-disabled my-2'} onClick={onClick} disabled={disabled}>
       <FontAwesomeIcon className="text-lg mr-2" icon={icon} />
       <p>{text}</p>
     </button>
