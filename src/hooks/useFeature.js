ff69c02f5e051a40ea71f9594fa7cb4024588a8a
Jimmy McBroom <james.mcbroom@gmail.com>
2021-04-21 22:38:42 -0400

    document useFeature hook

--- src/hooks/useFeature.js
+++ src/hooks/useFeature.js
@@ -12,17 +12,27 @@ import layers from '../data/layers.js'
  * @returns an ArcJSON item
  */
 const useFeature = ({ type, id }) => {
+
+  // we store the returned data here
   let [data, setData] = useState(null)
 
+  // initialize empty variables
   let layer, where;
+
+  // if we have a type:
   if (type) {
     layer = type ? layers[type] : null
+    // if that layer is parcels
     where = type === 'parcels'
+      // wrap the id in single-quotes
       ? `${layer.id_column} = '${id}'`
+      // if not parcels, no quotes
       : `${layer.id_column} = ${id}`
   }
 
+  // this data fetch will fire any time we change the underlying object
   useEffect(() => {
+    // assuming we have a type & id:
     if (type && id) {
       queryFeatures({
         url: layer.endpoint,
@@ -30,10 +40,9 @@ const useFeature = ({ type, id }) => {
         'outFields': '*',
         'resultRecordCount': 1,
         'outSR': 4326
+      }).then(d => {
+        setData(d.features[0])
       })
-        .then(d => {
-          setData(d.features[0])
-        })
     }
   }, [type, id])
 
