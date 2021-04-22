e361df163fbcb14fe7190c5ec340117be418a44f
Jimmy McBroom <james.mcbroom@gmail.com>
2021-04-21 20:46:15 -0400

    create useFeature custom hook

---
+++ src/hooks/useFeature.js
@@ -0,0 +1,34 @@
+import { useState, useEffect } from 'react'
+import { queryFeatures } from '@esri/arcgis-rest-feature-layer';
+import layers from '../data/layers.js'
+
+const useFeature = ({ type, id }) => {
+  let [data, setData] = useState(null)
+
+  let layer, where;
+  if (type) {
+    layer = type ? layers[type] : null
+    where = type === 'parcels'
+      ? `${layer.id_column} = '${id}'`
+      : `${layer.id_column} = ${id}`
+  }
+
+  useEffect(() => {
+    if (type && id) {
+      queryFeatures({
+        url: layer.endpoint,
+        'where': where,
+        'outFields': '*',
+        'resultRecordCount': 1,
+        'outSR': 4326
+      })
+        .then(d => {
+          setData(d.features[0])
+        })
+    }
+  }, [type, id])
+
+  return data
+}
+
+export default useFeature;
