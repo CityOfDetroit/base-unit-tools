81be83e492f4f3c28f7c2f437ce2b6d56655744d
Jimmy McBroom <james.mcbroom@gmail.com>
2021-04-22 16:17:08 -0400

    comments on LinkerMap

--- src/Linker/LinkerMap.js
+++ src/Linker/LinkerMap.js
@@ -8,6 +8,7 @@ const LinkerMap = ({ center, feature, links, setLinks }) => {
 
   const [theMap, setTheMap] = useState(null);
 
+  // we keep a local copy of the to-be-submitted IDs here in these three states
   let [parcel, setParcel] = useState(null)
   let [building, setBuilding] = useState(null)
   let [street, setStreet] = useState(null)
@@ -24,6 +25,8 @@ const LinkerMap = ({ center, feature, links, setLinks }) => {
 
     map.on("load", () => {
       setTheMap(map);
+      
+      // set the initial map filters based on the feature's properties.
       map.setFilter("building-highlight", ["==", "$id", feature.properties.bldg_id])
       map.setFilter("parcel-highlight", ["==", "parcel_id", feature.properties.parcel_id])
       map.setFilter("address-highlight", ["==", "$id", feature.properties.addr_id])
@@ -31,11 +34,14 @@ const LinkerMap = ({ center, feature, links, setLinks }) => {
     });
 
     map.on('click', e => {
+      // get all the features in these layers
       let features = map.queryRenderedFeatures(e.point, {
         layers: ['building-fill', 'parcel-fill', 'streets-line'],
       });
+      // if we have clicked on any features..
       if (features.length > 0) {
         let ft = features[0]
+        // depending the type, set a new state.
         if(ft.source === 'buildings') {
           setBuilding(ft.id)
         }
@@ -52,11 +58,13 @@ const LinkerMap = ({ center, feature, links, setLinks }) => {
     })
   }, []);
 
+  // this effect fires when any of our three states change
   useEffect(() => {
     if(parcel && theMap) {
+      // set the links object to the new parcel ID
       links.parcel_id = parcel
+      // set the new filter on the map layer
       theMap.setFilter("parcel-highlight", ["==", "parcel_id", parcel])
-
     }
     if(building && theMap) {
       theMap.setFilter("building-highlight", ["==", "$id", building])
@@ -66,6 +74,8 @@ const LinkerMap = ({ center, feature, links, setLinks }) => {
       links.street_id = street
       theMap.setFilter("streets-highlight", ["==", "street_id", street])
     }
+
+    // finally, set the new links.
     setLinks({
       ...links
     })
