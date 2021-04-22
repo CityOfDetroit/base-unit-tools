4c0039a485f95c4ae9f4d09bbfe96f9ad8e0b753
Jimmy McBroom <james.mcbroom@gmail.com>
2021-04-22 13:55:50 -0400

    comments on useGeocoder

--- src/hooks/useGeocoder.js
+++ src/hooks/useGeocoder.js
@@ -20,23 +20,29 @@ const useGeocoder = (input) => {
 
   useEffect(() => {
     if (input) {
+
+      // first, we try to call the base units geocoder
       geocode({
         endpoint: geocoderUrl,
         singleLine: input,
         outFields: '*'
       }).then(r => {
+        // if we got a result, return it.
         if (r.candidates.length > 0) {
           setData(r.geoJson)
           setResultType('point')
         }
+        // we did not get a result: fall back to the street centerline geocoder
         else {
           let spacesRe = /\s/g
+          // which does not seem to be supported by the Esri library, so we manually fetch using this URL template
           let url = `https://gis.detroitmi.gov/arcgis/rest/services/DoIT/StreetCenterlineGeocoder/GeocodeServer/findAddressCandidates?Single+Line+Input=${input.replace(spacesRe, '+')}&outFields=*&outSR=4326&f=pjson`
           fetch(url)
             .then(s => s.json())
             .then(d => {
               if (d.candidates.length > 0) {
                 let add = d.candidates[0]
+                // manually creating a FeatureCollection here
                 setData({
                   type: 'FeatureCollection',
                   features: [
@@ -52,6 +58,7 @@ const useGeocoder = (input) => {
                 })
                 setResultType('centerline')
               }
+              // if there was no match on either geocoder, we return [null, 'none']
               else {
                 setData(null)
                 setResultType('none')
@@ -62,6 +69,7 @@ const useGeocoder = (input) => {
     }
   }, [input])
 
+  // return the featureCollection and the string with the match type
   return [data, resultType]
 }
 
