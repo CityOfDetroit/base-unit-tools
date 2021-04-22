1948d4a8c54ed3bad5cf1b69bd29703e7bd6acde
Jimmy McBroom <james.mcbroom@gmail.com>
2021-04-22 11:25:12 -0400

    add useQuery to Linker

--- src/Linker/Linker.js
+++ src/Linker/Linker.js
@@ -1,12 +1,20 @@
 import React, { useState } from 'react';
 import SiteSidebar from '../layout/SiteSidebar'
+import useQuery from '../hooks/useQuery'
 
 const Linker = () => {
+
+  // get some URL params
+  let query = useQuery()
+  let queryType = query.get("type")
+  let queryId = query.get("id")
+
   return (
     <>
       <SiteSidebar title="Linker">
         <section className="sidebar-section">
-          <h2>Address to create links to</h2>
+          <h2>Feature to create links to</h2>
+          {queryType} {queryId}
           <div className="flex items-center justify-between">
           </div>
         </section>
