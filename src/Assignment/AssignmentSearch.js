1f1997b6ec73f5a9c011ad513931f6d4f320a13a
Jimmy McBroom <james.mcbroom@gmail.com>
2021-07-29 09:30:01 -0400

    updates

--- src/Assignment/AssignmentSearch.js
+++ src/Assignment/AssignmentSearch.js
@@ -8,19 +8,19 @@ const AssignmentSearch = ({ setSearchValue, type }) => {
     let [value, setValue] = useState('')
   
     return (
-      <section className='sidebar-section'>
-        <h2 className="text-base flex items-center justify-between">
-          Search for an address:
-          {/* what if we couldn't find it? */}
-          {type && type === 'none' && 
-            <div className="flex items-end justify-start text-xs font-semibold bg-red-400 text-gray-700 px-4 py-1">
-              No results found!
-            </div>
-          }
-        </h2>
-        <div className="flex items-center justify-start text-sm mt-1 w-4/5">
+      <section className="bg-gray-300 py-2 px-3">
+        <div className="flex items-center justify-start text-sm mt-1">
+          <h2 className="text-base flex items-center justify-between">
+            Search for an address:
+            {/* what if we couldn't find it? */}
+            {type && type === 'none' && 
+              <div className="flex items-end justify-start text-xs font-semibold bg-red-400 text-gray-700 px-4 py-1">
+                No results found!
+              </div>
+            }
+          </h2>
           <input
-            className="p-2 w-full bg-"
+            className="p-2 w-1/2 ml-4 bg-"
             type="text"
             value={value}
             onChange={(e) => setValue(e.target.value)} 
--- src/hooks/useGeocoder.js
+++ src/hooks/useGeocoder.js
@@ -4,7 +4,7 @@ import { geocode } from '@esri/arcgis-rest-geocoding';
 let geocoders = {
   org: `https://opengis.detroitmi.gov/opengis/rest/services/BaseUnits/BaseUnitsGeocoder/GeocodeServer`,
   prod: `https://opengis.detroitmi.gov/opengis/rest/services/BaseUnits/BaseUnitGeocoderWUnitsTest/GeocodeServer`,
-  dev: `https://opengis.detroitmi.gov/opengis/rest/services/BaseUnits/BaseUnitGeocoder/GeocodeServer/`
+  dev: `https://opengis.detroitmi.gov/opengis/rest/services/Geocoders/TestingJoinIDHypothesisBetaCOmposite/GeocodeServer/`
 }
 
 export const geocoderUrl = geocoders.dev
