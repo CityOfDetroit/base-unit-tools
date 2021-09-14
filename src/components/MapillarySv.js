9310ca6ce1b2b1ac055fa30a6638d188e2df8c69
Jimmy McBroom <james.mcbroom@gmail.com>
2021-09-14 09:11:04 -0400

    add feature marker back

--- src/components/MapillarySv.js
+++ src/components/MapillarySv.js
@@ -1,9 +1,27 @@
-import { Viewer } from "mapillary-js";
+import { Viewer, SimpleMarker } from "mapillary-js";
 import { useEffect, useState } from "react";
 import moment from "moment";
+import { arcgisToGeoJSON } from '@esri/arcgis-to-geojson-utils';
+import centroid from '@turf/centroid';
+
+const featureToCentroidCoords = (feature) => {
+  let geojsonFeature = arcgisToGeoJSON(feature)
+  let featureCentroid = centroid(geojsonFeature.geometry).geometry.coordinates
+  let coords = {lng: featureCentroid[0], lat: featureCentroid[1]}
+  return coords
+}
+
+let markerStyle = {
+  ballColor: "white",
+  ballOpacity: 0.65,
+  color: "#feb70d",
+  opacity: 0.55,
+  interactive: false,
+  radius: 2,
+};
 
 
-const MapillarySv = ({ svKeys, svImageKey, setSvImageKey, setSvBearing }) => {
+const MapillarySv = ({ svKeys, svImageKey, setSvImageKey, setSvBearing, feature }) => {
 
   let [streetview, setStreetview] = useState(null)
 
@@ -11,6 +29,15 @@ const MapillarySv = ({ svKeys, svImageKey, setSvImageKey, setSvBearing }) => {
     const viewer = new Viewer({
       accessToken: 'MLY|4690399437648324|de87555bb6015affa20c3df794ebab15',
       container: 'mly-viewer',
+      component: {
+        marker: true,
+        bearing: false,
+        cover: true,
+        attribution: false,
+        sequence: false,
+        cache: true,
+        direction: true
+      },
       imageId: svKeys[0].id.toString()
     });
 
@@ -18,8 +45,14 @@ const MapillarySv = ({ svKeys, svImageKey, setSvImageKey, setSvBearing }) => {
 
     setStreetview(viewer)
 
+    if(feature) {
+      let coords = featureToCentroidCoords(feature);
+      let defaultMarker = new SimpleMarker("default-id", { lat: coords.lat, lng: coords.lng }, markerStyle);
+      let markerComponent = viewer.getComponent("marker");
+      markerComponent.add([defaultMarker]);
+    }
+
     viewer.on("image", function(e) { 
-      console.log(`viewer.on`, e.image)
       setSvImageKey({id: e.image.id, captured_at: e.image._spatial.captured_at})
       e.target.getBearing()
         .then(d => setSvBearing(d))
@@ -36,6 +69,15 @@ const MapillarySv = ({ svKeys, svImageKey, setSvImageKey, setSvBearing }) => {
       streetview.moveTo(svKeys[0].id.toString())
     }
   }, [svKeys])
+
+  useEffect(() => {
+    if(streetview) {
+      let coords = featureToCentroidCoords(feature);
+      let defaultMarker = new SimpleMarker("default-id", { lat: coords.lat, lng: coords.lng }, markerStyle);
+      let markerComponent = streetview.getComponent("marker");
+      markerComponent.add([defaultMarker]);
+    }
+  }, [feature])
 
   return (
     <>
