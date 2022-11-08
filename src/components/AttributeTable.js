d0938c325bc533e5407ac9b48853bcc124dff204
Kevin Zhang <Kevin.Zhang@detroitmi.gov>
2022-11-08 12:26:01 -0500

    paused on tooltip

--- src/components/AttributeTable.js
+++ src/components/AttributeTable.js
@@ -5,36 +5,37 @@ import AnimateHeight from 'react-animate-height'
 import CopyValue from "../components/CopyValue";
 import CustomTooltip from "./CustomTooltip";
 
-const AttributeTable = ({ attributes, metadata = null}) => {
+const AttributeTable = ({ attributes, metadata = {}}) => {
   console.log(attributes)
   console.log(metadata)
   
-  const TableRow = (f, i) => {    
+  const TableRow = ({label, index}) => {   
+    console.log(metadata?.[label])
     return (
       <tr
-          key={i}
+          key={index}
           className={
-            i + 1 === Object.keys(attributes).length
+            index + 1 === Object.keys(attributes).length
               ? "flex items-center"
               : "border-b-2 border-gray-400 flex items-center"
           }
         >
           {/* If the data is a * marked field, don't display the star */}
-          {f.slice(-1) == "*" ? (
-            <td className="w-1/3 md:w-2/5 my-2 font-bold text-xs md:text-sm ">{f.slice(0, -1)}</td>
+          {label.slice(-1) == "*" ? (
+            <td className="w-1/3 md:w-2/5 my-2 font-bold text-xs md:text-sm ">{label.slice(0, -1)}</td>
           ) : (
-            <td className="w-1/3 md:w-2/5 my-2 font-bold text-xs md:text-sm ">{f}</td>
+            <td className="w-1/3 md:w-2/5 my-2 font-bold text-xs md:text-sm ">{label}</td>
           )
           }
           <td className="text-xs md:text-sm flex w-2/3 md:w-3/5 my-2 justify-between items-center pr-2">
-          {f.slice(-1) == "*" ? (
-              convertTimestamp(attributes[f])
+          {label.slice(-1) == "*" ? (
+              convertTimestamp(attributes[label])
             ) : (
-              attributes[f]
+              attributes[label]
             )
           }
-            {attributes[f] && attributes[f] !== "" && (
-              <CopyValue value={attributes[f]} className="text-gray-300 hover:text-gray-400" />
+            {attributes[label] && attributes[label] !== "" && (
+              <CopyValue value={attributes[label]} className="text-gray-300 hover:text-gray-400" />
             )}
           </td>
         </tr>
@@ -52,7 +53,11 @@ const AttributeTable = ({ attributes, metadata = null}) => {
     <table className="w-full">
       <tbody>
         {Object.keys(attributes).map((f, i) => (
-          TableRow(f, i)
+          metadata?.[f]
+          ? <CustomTooltip key={i} title={metadata[f]}>
+              <TableRow label={f} index={i} />
+            </CustomTooltip>
+          :<TableRow label={f} index={i} />
         ))}
       </tbody>
     </table>
